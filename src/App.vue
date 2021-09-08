a3fa1f2f31f01ed9ea5caf6dd25b268f9636900b
kasukajima <zetta_minamimoto@softbank.ne.jp>
2021-09-08 15:55:02 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
   <div id="app">
-    <HelloWorld v-bind:title="message" v-on:result-event = "appAction" />
-    <hr>
-    <p>{{ result }}</p>
+    <mondai title="なぞなぞ" v-bind:message="que"/>
+    <div class="alert">
+    <h3 ref="title" class="title" style="text-align:center;width:400px;margin:30px auto 0 auto">選択肢</h3>
+    </div>
+    <sentaku v-bind:select="ans.A" v-bind:answer='false' number="A" />
+    <sentaku v-bind:select="ans.B" v-bind:answer='false' number="B" />
+    <sentaku v-bind:select="ans.C" v-bind:answer='false' number="C" />
+    <sentaku v-bind:select="ans.D" v-bind:answer='true' number="D" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import mondai from './components/mondai.vue';
+import sentaku from './components/sentaku.vue';
 
 export default {
   name:"App",
   components:{
-    HelloWorld
+    mondai,
+    sentaku,
   },
   data(){
     return {
-      message:"HELLO",
-      result:"no event."
+      que:"根は植物。本は楽器。炭は回転。では盆は？",
+      ans:{
+        A:"三国志",
+        B:"太陽系",
+        C:"化学式",
+        D:"体組織"
+      }
     }
-  },
-  methods: {
-    appAction(message){
-      this.result = '(*** you send:"' + message + '".***)'
-    }
-  },
+  }
 }
 
 </script>
